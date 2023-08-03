de6d2aa10946b65029be509f48c97300348b7081
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 01:29:56 +0330

    Create HooksCakeContainer.jsx

---
+++ react-redux-example/src/components/HooksCakeContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HooksCakeContainer() {
+  return (
+    <div>
+      <h2>number of cakes : </h2>
+      <button>buyCake</button>
+    </div>
+  );
+}
+
+export default HooksCakeContainer;
