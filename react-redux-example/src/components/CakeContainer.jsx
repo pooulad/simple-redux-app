1f0a30f48fa31c2d8f63e450a40c8f60d12196bd
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 01:15:54 +0330

    Update CakeContainer.jsx

--- react-redux-example/src/components/CakeContainer.jsx
+++ react-redux-example/src/components/CakeContainer.jsx
@@ -3,11 +3,16 @@ import React, { memo } from "react";
 function CakeContainer() {
   return (
     <div>
-      <div>CakeContainer</div>
-      <div>CakeContainer</div>
-      <div>CakeContainer</div>
+      <h2>number of cakes : </h2>
+      <button>buy cake</button>
     </div>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    numberOfCakes: state.numberOfCakes,
+  };
+};
 
 export default memo(CakeContainer);
