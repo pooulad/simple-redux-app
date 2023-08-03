57f69efdfe50e65b03c91fa32fe39776428a59dd
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 01:36:28 +0330

    add useSelector and useDispatch for test functional component too

--- react-redux-example/src/App.jsx
+++ react-redux-example/src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
       </div>
     </Provider>
--- react-redux-example/src/components/HooksCakeContainer.jsx
+++ react-redux-example/src/components/HooksCakeContainer.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { buyCake } from "../redux";
 
 function HooksCakeContainer() {
+  const numberOfCakes = useSelector((state) => state.numberOfCakes);
+  const buyCakeDispatch = useDispatch();
   return (
     <div>
-      <h2>number of cakes : </h2>
-      <button>buyCake</button>
+      <h2>number of cakes : {numberOfCakes}</h2>
+      <button
+        onClick={() => {
+          buyCakeDispatch(buyCake());
+        }}
+      >
+        buyCake
+      </button>
     </div>
   );
 }
