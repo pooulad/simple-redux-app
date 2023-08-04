ce809e162bf436fb77195b1b8f38df156aefd2a4
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 14:19:26 +0330

    Create iceCreamReducer.js

---
+++ react-redux-example/src/redux/iceCream/iceCreamReducer.js
@@ -0,0 +1,19 @@
+import { BUY_ICECREAM } from "./iceCreamTypes";
+
+const initialState = {
+    numberOfCakes: 20,
+}
+
+const iceCreamReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_ICECREAM:
+            return {
+                ...state,
+                numberOfIceCreams: state.numberOfIceCreams - 1
+            }
+        default:
+            return state
+    }
+}
+
+export default iceCreamReducer;
