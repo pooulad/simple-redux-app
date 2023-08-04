455e97680a22ce6ce7fb6c482caf860415423e10
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 14:52:03 +0330

    Update store.js

--- react-redux-example/src/redux/store.js
+++ react-redux-example/src/redux/store.js
@@ -1,8 +1,12 @@
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from '@redux-devtools/extension';
 import rootReducer from "./rootReducer";
 import logger from "redux-logger";
 
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(logger)
+    // other store enhancers if any
+));
 
 export default store;
