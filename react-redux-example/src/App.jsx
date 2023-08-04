e005cc5c5adfe88b1f3d237b04a108cf7c7f066b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 15:00:04 +0330

    Update App.jsx

--- react-redux-example/src/App.jsx
+++ react-redux-example/src/App.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <HooksCakeContainer />
         <CakeContainer />
+        <NewCakeContainer />
         <IceCreamContainer />
       </div>
     </Provider>
