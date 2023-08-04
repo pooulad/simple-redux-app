8a853c66638f6bf929483d91c59b5006760b4df0
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 14:23:06 +0330

    combine all reducers in root reducer file

--- react-redux-example/src/redux/store.js
+++ react-redux-example/src/redux/store.js
@@ -1,7 +1,7 @@
 import { legacy_createStore as createStore } from "redux";
-import cakeReducer from "./cake/cakeReducer";
+import rootReducer from "./rootReducer";
 
 
-const store = createStore(cakeReducer);
+const store = createStore(rootReducer);
 
 export default store;
