5062d9565727deb48ff1c843381f74ac563d6695
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 16:15:44 +0330

    Update ItemContainer.jsx

--- react-redux-example/src/components/ItemContainer.jsx
+++ react-redux-example/src/components/ItemContainer.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import { buyCake } from "../redux";
+import { buyIceCream } from "../redux/iceCream/iceCreamAction";
 
 function ItemContainer(props) {
   return (
     <div>
       <h2>item - {props.item}</h2>
+      <button onClick={props.buyItem}>buy</button>
     </div>
   );
 }
@@ -19,4 +22,14 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+
+  return {
+    buyItem: dispatchFunction,
+  };
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(ItemContainer);
