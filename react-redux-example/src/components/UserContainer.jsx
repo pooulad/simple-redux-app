a2c49a4fd4114669d6c8f14d4eab0716d4076169
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 16:42:20 +0330

    Update UserContainer.jsx

--- react-redux-example/src/components/UserContainer.jsx
+++ react-redux-example/src/components/UserContainer.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../redux";
 
-function UserContainer() {
+function UserContainer({ userData, fetchUsers }) {
+  useEffect(() => {
+    fetchUsers();
+  }, []);
   return (
     <div>
-      <div>UserContainer</div>
-      <div>UserContainer</div>
-      <div>UserContainer</div>
+      {userData.loading ? (
+        <div>loading...</div>
+      ) : userData.error ? (
+        <div>{userData.error}/</div>
+      ) : (
+        <div>
+          {userData && userData.users?.map((user) => (
+            <div>{user.id}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
-export default UserContainer;
+const mapStateToProps = (state) => {
+  return {
+    userData: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUsers: () => dispatch(fetchUsers()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
