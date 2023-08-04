0c0e5e804a89234f882ccd8f35291c6454ef9c0c
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-04 16:24:48 +0330

    Create UserContainer.jsx

---
+++ react-redux-example/src/components/UserContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UserContainer() {
+  return (
+    <div>
+      <div>UserContainer</div>
+      <div>UserContainer</div>
+      <div>UserContainer</div>
+    </div>
+  );
+}
+
+export default UserContainer;
